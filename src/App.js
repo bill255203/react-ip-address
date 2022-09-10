02c138d168264aa524773928143cadc6b0e707b9
bill <bill255203@gmail.com>
2022-09-11 00:18:37 +0800

    9/11 fetched data

--- src/App.js
+++ src/App.js
@@ -1,31 +1,61 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [name, setName] = useState('');
-
+  const [ip, setIp] = useState('');
+  const [location, setLocation] = useState([]);
+  const [isp, setIsp] = useState('');
+  useEffect(() => {
+    const url = `https://geo.ipify.org/api/v2/country?apiKey=at_KZyspMc5zCFbRiv8DcQ4ZLu9nRFrY&ipAddress=${ip}`;
+    console.log(url);
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setIp(json.ip);
+        setLocation(json.location);
+        setIsp(json.isp);
+      })
+      .catch((rejected) => {
+        console.log(rejected);
+      });
+  }, [ip]);
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(event.target.value);
+    const newIP = event.target[0].value;
+    console.log(newIP);
+    setIp(newIP);
   };
   return (
     <div className='App'>
       <h1 className='title'>IP address tracker</h1>
       <form onSubmit={handleSubmit}>
-        <label>
-          Enter IP:
-          <input
-            type='text'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <input type='submit' value='submit' />
+        <input
+          type='text'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input type='submit' value='>' />
       </form>
       <div className='information'>
-        <div className='info'></div>
-        <div className='info'></div>
-        <div className='info'></div>
-        <div className='info'></div>
+        <div className='info'>
+          <div className='title'>IP:</div>
+          <p>{ip}</p>
+        </div>
+        <div className='info'>
+          <div className='title'>LOCATION:</div>
+          <p>
+            {location.country}, {location.region}
+          </p>
+        </div>
+        <div className='info'>
+          <div className='title'>TIME:</div>
+          <p>{location.timezone}</p>
+        </div>
+        <div className='info'>
+          <div className='title'>ISP:</div>
+          <p>{isp}</p>
+        </div>
       </div>
     </div>
   );
